0d66f5dc99b944682d9d457f250c96e51a0e90ea
amanoese <amanoese@gmail.com>
2019-10-18 17:12:34 +0900

    add kpoint initiraizer

--- pkg/kmean.go
+++ pkg/kmean.go
@@ -1,25 +1,61 @@
 package pkg
 
+import (
+  "fmt"
+  "math"
+  "math/big"
+  "math/rand"
+  crand "crypto/rand"
+)
+
 type Point struct {
-  values []int
+	values []int
+}
+
+func (point *Point) setValues(values []int) {
+    newValues := make([]int,len(values))
+    copy(newValues,values)
+    point.values = newValues
 }
 
 type CategorizedPoint struct {
-  values []int
-  category []int
+	values []int
+	category []int
 }
 
-func Calc(points []Point) []CategorizedPoint {
-  cpoints := []CategorizedPoint{}
+func Init_kpoint(points []Point,k int) []Point {
 
-  for _, point := range points {
+  _points := make([]Point,len(points))
+  copy(_points,points)
+  kpoints := make([]Point,k)
 
-    cpoint := CategorizedPoint{
-      values:   point.values,
-      category: point.values,
-    }
+  seed, _ := crand.Int(crand.Reader, big.NewInt(math.MaxInt64))
+  rand.Seed(seed.Int64())
 
-    cpoints = append(cpoints,cpoint)
+  for i :=0; i < k ; i++ {
+    use_index := rand.Intn(len(_points) - 1)
+    fmt.Printf("use_index:%d\n",use_index)
+
+    var kpoint Point;
+    kpoint.setValues(_points[use_index].values)
+    kpoints[i] = kpoint
+
+    _points = append(_points[:use_index],_points[use_index+1:]...)
   }
-  return cpoints
+  return kpoints
+}
+
+func Calc(points []Point,k int) []CategorizedPoint {
+	cpoints := []CategorizedPoint{}
+
+	for _, point := range points {
+
+		cpoint := CategorizedPoint{
+			values:   point.values,
+			category: point.values,
+		}
+
+		cpoints = append(cpoints,cpoint)
+	}
+	return cpoints
 }
--- pkg/kmean_test.go
+++ pkg/kmean_test.go
@@ -1,19 +1,34 @@
 package pkg
 
 import (
-    "fmt"
-    "testing"
+	"fmt"
+	"testing"
 )
 
+func TestInitKpoint(t *testing.T) {
+  v1 := Point{ values: []int{1} }
+  v2 := Point{ values: []int{2} }
+  v3 := Point{ values: []int{3} }
+
+	points := []Point{v1,v2,v3}
+  kpoints := Init_kpoint(points,1)
+
+  t.Log(kpoints)
+
+	if len(kpoints) != 1 {
+		t.Fatal("failed test")
+  }
+}
+
 func TestExampleSuccess(t *testing.T) {
-    v1 := Point{ values: []int{1,2} }
-    v2 := Point{ values: []int{1,2} }
+  v1 := Point{ values: []int{1,2} }
+	v2 := Point{ values: []int{1,2} }
 
-    points := []Point{v1,v2}
-    fmt.Print(points)
-    result := Calc(points)
+	points := []Point{v1,v2}
+	fmt.Print(points)
+	result := Calc(points,1)
 
-    if len(result) == 0 {
-        t.Fatal("failed test")
-    }
+	if len(result) == 0 {
+		t.Fatal("failed test")
+	}
 }
