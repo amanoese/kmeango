baaac08611757397ee459e809fa158d7fb3d5c7b
amanoese <amanoese@gmail.com>
2019-10-21 17:31:05 +0900

    add cmd

--- pkg/kmean.go
+++ pkg/kmean.go
@@ -1,7 +1,7 @@
 package pkg
 
 import (
-  "fmt"
+  //"fmt"
   "math"
   "math/big"
   "math/rand"
@@ -9,20 +9,29 @@ import (
 )
 
 type Point struct {
-	values []int
+	Values []int
 }
 
 func (point *Point) setValues(values []int) {
     newValues := make([]int,len(values))
     copy(newValues,values)
-    point.values = newValues
+    point.Values = newValues
 }
 
+//計算用のstruct
+type CategoryPoint struct {
+	Values []int
+	category int
+}
+
+//結果用のstruct
 type CategorizedPoint struct {
-	values []int
-	category []int
+	Values []int
+	CategoryNum int
+	CategoryValue []int
 }
 
+//クラスタ中心の初期化
 func Init_kpoint(points []Point,k int) []Point {
 
   _points := make([]Point,len(points))
@@ -32,12 +41,13 @@ func Init_kpoint(points []Point,k int) []Point {
   seed, _ := crand.Int(crand.Reader, big.NewInt(math.MaxInt64))
   rand.Seed(seed.Int64())
 
+  //pointsにある要素からランダムにk個の要素を取り出す
   for i :=0; i < k ; i++ {
     use_index := rand.Intn(len(_points) - 1)
-    fmt.Printf("use_index:%d\n",use_index)
+    //fmt.Printf("use_index:%d\n",use_index)
 
     var kpoint Point;
-    kpoint.setValues(_points[use_index].values)
+    kpoint.setValues(_points[use_index].Values)
     kpoints[i] = kpoint
 
     _points = append(_points[:use_index],_points[use_index+1:]...)
@@ -46,13 +56,57 @@ func Init_kpoint(points []Point,k int) []Point {
 }
 
 func Calc(points []Point,k int) []CategorizedPoint {
+  //クラスタ中心
+  clusters := Init_kpoint(points,k)
+
+  //fmt.Println(clusters)
+  //適当に初期化
+  clusteryPoints := make([]CategoryPoint,cap(points))
+	for i, point := range points {
+    clusteryPoints[i] = CategoryPoint{ point.Values, 0 }
+  }
+
+  for loop := 0 ; loop < 100 ; loop = loop + 1 {
+    for cp_i, point := range clusteryPoints {
+      min := math.MaxFloat64
+
+      for num, cluster_point := range clusters {
+         diff := 0.0
+         for ci, cvalue := range cluster_point.Values {
+            diff = diff + math.Pow(float64(point.Values[ci] - cvalue),2)
+         }
+         if diff < min  {
+            min = diff
+            clusteryPoints[cp_i].category = num
+         }
+      }
+    }
+    for ci, cluster_point := range clusters {
+      count := 0
+      sump   := make([]int,cap(cluster_point.Values))
+      for _, point := range clusteryPoints {
+        if ci == point.category {
+          count = count + 1
+          for i, sum:= range sump {
+            sump[i] = sum + point.Values[i]
+          }
+        }
+      }
+      for i, sum:= range sump {
+        sump[i] = int(sum/count)
+      }
+      clusters[ci].Values = sump
+    }
+  }
+
 	cpoints := []CategorizedPoint{}
 
-	for _, point := range points {
+	for _, point := range clusteryPoints {
 
 		cpoint := CategorizedPoint{
-			values:   point.values,
-			category: point.values,
+			Values:   point.Values,
+			CategoryNum: point.category,
+			CategoryValue: clusters[point.category].Values,
 		}
 
 		cpoints = append(cpoints,cpoint)
--- pkg/kmean_test.go
+++ pkg/kmean_test.go
@@ -2,13 +2,14 @@ package pkg
 
 import (
 	"fmt"
+  "strconv"
 	"testing"
 )
 
 func TestInitKpoint(t *testing.T) {
-  v1 := Point{ values: []int{1} }
-  v2 := Point{ values: []int{2} }
-  v3 := Point{ values: []int{3} }
+  v1 := Point{ Values: []int{1} }
+  v2 := Point{ Values: []int{2} }
+  v3 := Point{ Values: []int{3} }
 
 	points := []Point{v1,v2,v3}
   kpoints := Init_kpoint(points,1)
@@ -21,12 +22,39 @@ func TestInitKpoint(t *testing.T) {
 }
 
 func TestExampleSuccess(t *testing.T) {
-  v1 := Point{ values: []int{1,2} }
-	v2 := Point{ values: []int{1,2} }
+  v1 := Point{ Values: []int{1,2} }
+	v2 := Point{ Values: []int{1,2} }
 
 	points := []Point{v1,v2}
 	fmt.Print(points)
 	result := Calc(points,1)
+
+	if len(result) == 0 {
+		t.Fatal("failed test")
+	}
+}
+
+func Test4meansSuccess(t *testing.T) {
+  points := make([]Point,4*20)
+
+  for i,_ := range points {
+    points[i] = Point{ Values: []int{i,i} }
+  }
+
+	fmt.Print(points)
+	result := Calc(points,4)
+
+  for _, point := range result {
+    category := ""
+    values := ""
+    for _,str := range point.CategoryValue {
+      category = category + " " + strconv.Itoa(str)
+    }
+    for _,str := range point.Values {
+      values = values + " " + strconv.Itoa(str)
+    }
+    //fmt.Println(category,":",values)
+  }
 
 	if len(result) == 0 {
 		t.Fatal("failed test")
